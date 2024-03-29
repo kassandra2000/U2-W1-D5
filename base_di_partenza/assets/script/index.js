4e499eafaefd1f089113dd7f7107b0591416215e
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-03-29 16:56:57 +0100

    extra

--- base_di_partenza/assets/script/index.js
+++ base_di_partenza/assets/script/index.js
@@ -1,13 +1,25 @@
 const button = document.querySelector(".first");
 const nav = document.querySelector("nav");
-
+const path = document.querySelectorAll("path");
 const scroll = window.addEventListener("scroll", (e) => {
-    if (window.scrollY > 430) {
-        nav.style.backgroundColor = "white";
-        button.style.backgroundColor = "green";
-    } else {nav.style.backgroundColor = "#ffc017";
-button.style.backgroundColor = "black";}
- 
+  if (window.scrollY > 430) {
+    nav.style.backgroundColor = "white";
+    button.style.backgroundColor = "green";
+  } else {
+    nav.style.backgroundColor = "#ffc017";
+    button.style.backgroundColor = "black";
+  }
 });
 
+const pathDisplay = function () {
+  for (let i = 0; i < path.length; i++) {
+    path[i].style.opacity = 0;
+    setTimeout(() => {
+      path[i].style.opacity = 1;
+    }, 1000);
+    console.log(path[i]);
+  }
+};
+pathDisplay()
 
+// setInterval(pathDisplay, 1000);

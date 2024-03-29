289143c72b49ff5cb01e9993d4ef51d88bb2cbd2
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-03-29 22:01:11 +0100

    ok

--- base_di_partenza/assets/script/index.js
+++ base_di_partenza/assets/script/index.js
@@ -1,6 +1,8 @@
 const button = document.querySelector(".first");
 const nav = document.querySelector("nav");
 const path = document.querySelectorAll("path");
+const arreyPath = [];
+const arreyPath2 = [];
 const scroll = window.addEventListener("scroll", (e) => {
   if (window.scrollY > 430) {
     nav.style.backgroundColor = "white";
@@ -11,15 +13,35 @@ const scroll = window.addEventListener("scroll", (e) => {
   }
 });
 
-const pathDisplay = function () {
-  for (let i = 0; i < path.length; i++) {
-    path[i].style.opacity = 0;
-    setTimeout(() => {
-      path[i].style.opacity = 1;
-    }, 1000);
-    console.log(path[i]);
-  }
-};
-pathDisplay()
+for (let i = 0; i < path.length; i++) {
+  arreyPath.push(path[i]);
+}
+const g = document.querySelectorAll("g");
+let j = 0;
 
-// setInterval(pathDisplay, 1000);
+for (let i = 0; i < arreyPath.length; i++) {
+  if (g[i].getAttribute("opacity") == "1") {
+    arreyPath2.push(g[i]);
+  }
+  
+}
+let k = 0;
+console.log(arreyPath2[1].getAttribute("opacity"));
+setInterval(() => {
+  if (g[j].getAttribute("opacity") == "1") {
+    arreyPath2.push(arreyPath[j]);
+    arreyPath[j].style.opacity = "0";
+    console.log(j);
+    if (j > 189) {
+      console.log("ciao");
+      j = 0;
+    }
+  } else {
+    arreyPath2[j].style.opacity = "1";
+    if (j > 280) {
+      j = 0;
+    }
+  }
+  j++;
+}, 20);
+
